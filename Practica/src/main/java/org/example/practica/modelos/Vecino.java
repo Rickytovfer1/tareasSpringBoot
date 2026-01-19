d17575e264bb91ffb9475ce28391fe7157ece11c
Ricardo <tovar.fernandez.ricardo@iescamas.es>
2026-01-19 11:14:52 +0100

    Vecino modelo y vecino repositorio hechos

---
+++ Practica/src/main/java/org/example/practica/modelos/Vecino.java
@@ -0,0 +1,59 @@
+package org.example.practica.modelos;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "vecino", schema = "practica_pablo")
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Vecino {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "apellidos", nullable = false)
+    private String apellidos;
+
+    @Column(name = "telefono", nullable = false)
+    private String telefono;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
---
+++ Practica/src/main/java/org/example/practica/repositorios/VecinoRepositorio.java
@@ -0,0 +1,8 @@
+package org.example.practica.repositorios;
+
+import org.example.practica.modelos.Vecino;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VecinoRepositorio extends JpaRepository<Vecino, Integer> {
+
+}
